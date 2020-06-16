96e025df6feb9d3e4ce8afe2f00a14cdeb2bab91
Scott Williams <scott@mightydevco.com>
2020-06-15 19:09:28 -0500

    certs controller cleaned up and almost working

--- go/src/test-operator/main_test.go
+++ go/src/test-operator/main_test.go
@@ -1,257 +1,132 @@
 package testoperator
 
 import (
-	"bytes"
-	"fmt"
 	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/retry"
 	"gopkg.in/yaml.v2"
-	"html/template"
 	"io/ioutil"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 	"time"
 )
 
-func getLoadBalancerIP() (string, error) {
-	rtn, found := os.LookupEnv("LOAD_BALANCER_IP")
-
-	if !found {
-		return "", fmt.Errorf("LOAD_BALANCER_IP must be set in order to perform FULL_TEST")
-	}
-	return rtn, nil
+type Stage struct {
+	Conf      *theConf
+	Templates *TemplatesStruct
+	Options   *k8s.KubectlOptions
 }
 
-func getDomain() (string, error) {
-	rtn, found := os.LookupEnv("DOMAIN")
+func createStage() *Stage {
 
-	if !found {
-		return "", fmt.Errorf("DOMAIN must be set in order to perform FULL_TEST")
+	conf := loadConf()
+	stage := Stage{
+		Conf:      conf,
+		Templates: fromAll(conf),
+		Options:   k8s.NewKubectlOptions("", "", conf.Namespace),
 	}
-	return rtn, nil
+	return &stage
 }
 
-func isFullTest() bool {
-	rtn, found := os.LookupEnv("FULL_TEST")
-	if !found {
-		return false
-	}
-	return strings.TrimSpace(rtn) == "true"
+func (stage *Stage) unstage(t *testing.T) {
+	defer k8s.KubectlDeleteFromString(t, stage.Options, stage.Templates.ServiceTemplate)
+	defer k8s.KubectlDeleteFromString(t, stage.Options, stage.Templates.DeploymentTemplate)
+	defer k8s.KubectlDeleteFromString(t, stage.Options, stage.Templates.SslProxyTemplate)
 }
 
-func getName() string {
+/**
+ * here we stage the environment which involves setting up the SSLProxy
+ */
+func (stage *Stage) stage(t *testing.T) {
 
-	rtn, found := os.LookupEnv("NAME")
-	if !found {
-		rtn = "test"
+	if stage.Conf.StageTeardown {
+		stage.unstage(t)
 	}
-	return rtn
+
+	k8s.KubectlApplyFromString(t, stage.Options, stage.Templates.ServiceTemplate)
+	k8s.KubectlApplyFromString(t, stage.Options, stage.Templates.DeploymentTemplate)
+
+	// before we create an SSLProxy the service it points to must be available
+	// otherwise NGINX will crash
+	k8s.WaitUntilServiceAvailable(t, stage.Options, stage.Conf.Name, 60, time.Second*5)
+
+	k8s.KubectlApplyFromString(t, stage.Options, stage.Templates.SslProxyTemplate)
+
+	k8s.WaitUntilNumPodsCreated(t, stage.Options, metav1.ListOptions{LabelSelector: "app.kubernetes.io/name=" + stage.Conf.Name}, 1, 60, time.Second*10)
+	k8s.WaitUntilNumPodsCreated(t, stage.Options, metav1.ListOptions{LabelSelector: "sslproxy=" + stage.Conf.Name}, 1, 60, time.Second*10)
+
+	// wait for services that the operator should create
+	k8s.WaitUntilServiceAvailable(t, stage.Options, stage.Conf.Name+"-nginx-sslproxy", 30, time.Second*10)
+	k8s.WaitUntilServiceAvailable(t, stage.Options, stage.Conf.Name+"-certbot-service", 30, time.Second*10)
 }
 
-func getNameSpace() string {
-
-	rtn, found := os.LookupEnv("NAMESPACE")
-	if !found {
-		rtn = "default"
+func (stage *Stage) testExpectedSslProxyDefaultConf(t *testing.T) {
+	pods := k8s.ListPods(t, stage.Options, metav1.ListOptions{LabelSelector: "sslproxy=" + stage.Conf.Name})
+	pod := pods[0]
+	k8s.WaitUntilPodAvailable(t, stage.Options, pod.Name, 4, time.Second*15)
+	defaultConfig, err := k8s.RunKubectlAndGetOutputE(t, stage.Options, "exec", pod.Name, "--", "cat", "/etc/recert/conf/default.conf")
+	if err != nil {
+		t.Error(err)
+		t.Error("expected defaultConfig to be located at /etc/recert/conf/default.conf")
+		t.Fail()
 	}
-	return rtn
+	if !strings.Contains(defaultConfig, "NGINX TEMPLATE FROM RECERT OPERATOR") {
+		t.Error("expected header text in default.conf file not found: 'NGINX TEMPLATE FROM RECERT OPERATOR'")
+		t.Fail()
+	}
+}
+
+func (stage *Stage) testProxyWorking(t *testing.T) {
+	service := k8s.GetService(t, stage.Options, stage.Conf.Name+"-nginx-sslproxy")
+	ipAddress := k8s.GetServiceEndpoint(t, stage.Options, service, 80)
+	http_helper.HttpGetWithRetryWithCustomValidation(t, "http://"+ipAddress, nil, 30, 5*time.Second, func(code int, body string) bool {
+		return code == 200 && strings.Contains(body, "Hello World")
+	})
 }
 
 func TestSslProxy(t *testing.T) {
 	t.Parallel()
 
-	// Path to the Kubernetes resource config we will test.
-	sslProxyTemplate := `apiVersion: mightydevco.com/v1alpha1
-kind: SSLProxy
-metadata:
-  name: {{ .Name }}
-spec:
-  {{- if .IsFullTest }}
-  loadBalancerIP: {{ .LoadBalancerIP }}
-  {{- end }} 
-  reverseProxy: http://{{ .Name }}:80
-  replicas: 1`
+	stage := createStage()
+	stage.stage(t)
 
-	certTemplate := `apiVersion: mightydevco.com/v1alpha1
-kind: Cert
-metadata:
-  name: {{ .Name }}
-spec:
-  domain: {{ .Domain }}
-  email: scott@mightydevco.com
-  sslProxy: {{ .Name }}`
+	stage.testExpectedSslProxyDefaultConf(t)
+	stage.testProxyWorking(t)
+}
 
-	serviceTemplate := `apiVersion: v1
-kind: Service
-metadata:
-  name: {{ .Name }}
-  labels:
-    app.kubernetes.io/name: {{ .Name }}
-    app.kubernetes.io/instance: {{ .Name }}
-spec:
-  type: ClusterIP
-  ports:
-  - port: 80
-    targetPort: 80
-    protocol: TCP
-    name: http
-  selector:
-    app.kubernetes.io/name: {{ .Name }}
-    app.kubernetes.io/instance: {{ .Name }}
-`
+func TestCert(t *testing.T) {
+	stage := createStage()
+	stage.stage(t)
 
-	deploymentTemplate := `apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: {{ .Name }}
-  labels:
-    app.kubernetes.io/name: {{ .Name }}
-    app.kubernetes.io/instance: {{ .Name }}
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app.kubernetes.io/name: {{ .Name }}
-      app.kubernetes.io/instance: {{ .Name }}
-  template:
-    metadata:
-      labels:
-        app.kubernetes.io/name: {{ .Name }}
-        app.kubernetes.io/instance: {{ .Name }}
-    spec:
-      containers:
-        - name: nginx
-          image: nginxdemos/hello:latest
+	service := k8s.GetService(t, stage.Options, stage.Conf.Name+"-nginx-sslproxy")
+	ipAddress := k8s.GetServiceEndpoint(t, stage.Options, service, 80)
 
-          imagePullPolicy: Always
-          ports:
-            - name: http
-              containerPort: 80
-              protocol: TCP`
-
-	conf := theConf{
-		IsFullTest: isFullTest(),
-		Name:       getName(),
+	if ipAddress != stage.Conf.LoadBalancerIP {
+		t.Error("ip address != loadBalancerIP")
+		t.Fail()
+		return
 	}
 
-	if isFullTest() {
-		conf.LoadBalancerIP, _ = getLoadBalancerIP()
-		conf.Domain, _ = getDomain()
-	}
+	defer k8s.KubectlDeleteFromString(t, stage.Options, stage.Templates.CertTemplate)
+	k8s.KubectlApplyFromString(t, stage.Options, stage.Templates.CertTemplate)
 
-	var service string
-	{
-		tmpl, _ := template.New("service").Parse(serviceTemplate)
-		var w bytes.Buffer
-		tmpl.Execute(&w, conf)
-		service = string(w.Bytes())
-	}
+	k8s.WaitUntilNumPodsCreated(t, stage.Options, metav1.ListOptions{LabelSelector: "certbot=" + stage.Conf.Name}, 1, 30, time.Second*10)
 
-	var deployment string
-	{
-		tmpl, _ := template.New("deployment").Parse(deploymentTemplate)
-		var w bytes.Buffer
-		tmpl.Execute(&w, conf)
-		deployment = string(w.Bytes())
-	}
-
-	var sslProxy string
-	{
-		tmpl, _ := template.New("sslProxy").Parse(sslProxyTemplate)
-		var w bytes.Buffer
-		tmpl.Execute(&w, conf)
-		sslProxy = string(w.Bytes())
-	}
-
-	options := k8s.NewKubectlOptions("", "", getNameSpace())
-
-	// First the service must be setup or Nginx will crash
-	defer k8s.KubectlDeleteFromString(t, options, service)
-	defer k8s.KubectlDeleteFromString(t, options, deployment)
-	k8s.KubectlApplyFromString(t, options, service)
-	k8s.KubectlApplyFromString(t, options, deployment)
-	k8s.WaitUntilServiceAvailable(t, options, conf.Name, 60, time.Second*5)
-	k8s.WaitUntilNumPodsCreated(t, options, metav1.ListOptions{LabelSelector: "app.kubernetes.io/name=" + conf.Name}, 1, 60, time.Second*10)
-
-	// At the end of the test, run "kubectl delete" to clean up any resources that were created.
-	defer k8s.KubectlDeleteFromString(t, options, sslProxy)
-	k8s.KubectlApplyFromString(t, options, sslProxy)
-	k8s.WaitUntilNumPodsCreated(t, options, metav1.ListOptions{LabelSelector: "sslproxy=" + conf.Name}, 1, 60, time.Second*10)
-
-	pods := k8s.ListPods(t, options, metav1.ListOptions{LabelSelector: "sslproxy=" + conf.Name})
-	for i := 0; i < len(pods); i++ {
-		pod := pods[i]
-
-		k8s.WaitUntilPodAvailable(t, options, pod.Name, 4, time.Second*15)
-
-		defaultConfig, err := k8s.RunKubectlAndGetOutputE(t, options, "exec", pod.Name, "--", "cat", "/etc/recert/conf/default.conf")
-
-		if err != nil {
-			t.Error(err)
-			t.Error("expected defaultConfig to be located at /etc/recert/conf/default.conf")
-			t.Fail()
-		}
-
-		if !strings.Contains(defaultConfig, "NGINX TEMPLATE FROM RECERT OPERATOR") {
-			t.Error("expected header text in default.conf file not found: 'NGINX TEMPLATE FROM RECERT OPERATOR'")
-			t.Fail()
-		}
-	}
-
-	k8s.WaitUntilServiceAvailable(t, options, conf.Name+"-nginx-sslproxy", 30, time.Second*10)
-
-	k8s.WaitUntilServiceAvailable(t, options, conf.Name+"-certbot-service", 30, time.Second*10)
-
-	http_helper.HttpGetWithRetryWithCustomValidation(t, "http://"+conf.Domain, nil, 30, 5*time.Second, func(code int, body string) bool {
-
-		return code == 200 && strings.Contains(body, "Hello World")
-	})
-
-	if isFullTest() {
-
-		service := k8s.GetService(t, options, conf.Name+"-nginx-sslproxy")
-		ipAddress := k8s.GetServiceEndpoint(t, options, service, 80)
-
-		if ipAddress != conf.LoadBalancerIP {
-			t.Error("ip address != loadBalancerIP")
-			t.Fail()
-		}
-
-		var cert string
-		{
-			tmpl, _ := template.New("conf").Parse(certTemplate)
-			var w bytes.Buffer
-			tmpl.Execute(&w, conf)
-			cert = string(w.Bytes())
-		}
-		defer k8s.KubectlDeleteFromString(t, options, cert)
-		k8s.KubectlApplyFromString(t, options, cert)
-
-		k8s.WaitUntilNumPodsCreated(t, options, metav1.ListOptions{LabelSelector: "certbot=" + conf.Name}, 1, 30, time.Second*10)
-
+	if stage.Conf.IsFullTest {
 		// now make the same call using https
-		http_helper.HttpGetWithRetryWithCustomValidation(t, "https://"+conf.Domain, nil, 30, 5*time.Second, func(code int, body string) bool {
+		http_helper.HttpGetWithRetryWithCustomValidation(t, "https://"+stage.Conf.Domain, nil, 30, 5*time.Second, func(code int, body string) bool {
 
 			return code == 200 && strings.Contains(body, "Hello World")
 		})
-
 	}
-
-}
-
-func from(templateSrc string, data interface{}) string {
-	tmpl, _ := template.New("conf").Parse(templateSrc)
-	var w bytes.Buffer
-	tmpl.Execute(&w, data)
-	return string(w.Bytes())
 }
 
 func TestSecretsSSLCreate(t *testing.T) {
 	t.Parallel()
+
+	conf := loadConf()
 
 	deploymentTemplate := `apiVersion: v1
 kind: Pod
@@ -275,8 +150,7 @@ spec:
         containerPort: 80
         protocol: TCP`
 
-	options := k8s.NewKubectlOptions("", "", getNameSpace())
-	conf := loadConf()
+	options := k8s.NewKubectlOptions("", "", conf.Namespace)
 	{
 		deployment := from(deploymentTemplate, conf)
 		defer k8s.KubectlDeleteFromString(t, options, deployment)
@@ -311,36 +185,50 @@ spec:
 
 }
 
-func loadConf() theConf {
-	conf := theConf{
-		IsFullTest: isFullTest(),
-		Name:       getName(),
-	}
+/////////////////////////////////////////////
+// utility methods
+/////////////////////////////////////////////
 
-	if isFullTest() {
-		conf.LoadBalancerIP, _ = getLoadBalancerIP()
-		conf.Domain, _ = getDomain()
+func loadConf() *theConf {
+
+	var conf theConf
+	{
+		filename, _ := filepath.Abs("conf.yaml")
+		yamlFile, err := ioutil.ReadFile(filename)
+
+		err2 := yaml.Unmarshal(yamlFile, &conf)
+
+		if err != nil || err2 != nil {
+			conf.IsFullTest = false
+			conf.Namespace = "recert-test-namespace"
+			conf.Name = "test"
+			conf.StageTeardown = true
+		}
 	}
 
 	// requires that `make build` be run before this test
-	filename, _ := filepath.Abs("../../../out/images-manifest.yaml")
-	yamlFile, err := ioutil.ReadFile(filename)
+	{
+		filename, _ := filepath.Abs("../../../out/images-manifest.yaml")
+		yamlFile, err := ioutil.ReadFile(filename)
 
-	if err != nil {
-		panic("cannot load \"../../../out/images-manifest.yaml\" which must be built by \"make build\" at the root of this repository before this test can be run")
-	}
-	err = yaml.Unmarshal(yamlFile, &conf.Images)
-	if err != nil {
-		panic("cannot unmarshal \"../../../out/images-manifest.yaml\" which must be built by \"make build\" at the root of this repository before this test can be run")
+		if err != nil {
+			panic("cannot load \"../../../out/images-manifest.yaml\" which must be built by \"make build\" at the root of this repository before this test can be run")
+		}
+		err = yaml.Unmarshal(yamlFile, &conf.Images)
+		if err != nil {
+			panic("cannot unmarshal \"../../../out/images-manifest.yaml\" which must be built by \"make build\" at the root of this repository before this test can be run")
+		}
 	}
 
-	return conf
+	return &conf
 }
 
 type theConf struct {
-	IsFullTest     bool
-	LoadBalancerIP string
-	Domain         string
-	Name           string
-	Images         map[string]string
+	IsFullTest     bool              `json:"isFullTest"`
+	LoadBalancerIP string            `json:"loadBalancerIP"`
+	Domain         string            `json:"domain"`
+	Name           string            `json:"name"`
+	Namespace      string            `json:"namespace"`
+	StageTeardown  bool              `json:"stageTeardown"`
+	Images         map[string]string `json:"images,omitempty"`
 }
---
+++ go/src/test-operator/templates.go
@@ -0,0 +1,105 @@
+package testoperator
+
+import (
+	"bytes"
+	"text/template"
+)
+
+// TemplatesStruct holds all the templates
+type TemplatesStruct struct {
+	SslProxyTemplate   string
+	CertTemplate       string
+	ServiceTemplate    string
+	DeploymentTemplate string
+}
+
+// RawTemplates holds raw text templates
+var RawTemplates = TemplatesStruct{
+	// Path to the Kubernetes resource config we will test.
+	SslProxyTemplate: `apiVersion: mightydevco.com/v1alpha1
+kind: SSLProxy
+metadata:
+  name: {{ .Name }}
+spec:
+  {{- if .IsFullTest }}
+  loadBalancerIP: {{ .LoadBalancerIP }}
+  {{- end }} 
+  reverseProxy: http://{{ .Name }}:80
+  replicas: 1`,
+
+	CertTemplate: `apiVersion: mightydevco.com/v1alpha1
+kind: Cert
+metadata:
+  name: {{ .Name }}
+spec:
+  domain: {{ .Domain }}
+  email: scott@mightydevco.com
+  sslProxy: {{ .Name }}`,
+
+	ServiceTemplate: `apiVersion: v1
+kind: Service
+metadata:
+  name: {{ .Name }}
+  labels:
+    app.kubernetes.io/name: {{ .Name }}
+    app.kubernetes.io/instance: {{ .Name }}
+spec:
+  type: ClusterIP
+  ports:
+  - port: 80
+    targetPort: 80
+    protocol: TCP
+    name: http
+  selector:
+    app.kubernetes.io/name: {{ .Name }}
+    app.kubernetes.io/instance: {{ .Name }}
+`,
+
+	DeploymentTemplate: `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: {{ .Name }}
+  labels:
+    app.kubernetes.io/name: {{ .Name }}
+    app.kubernetes.io/instance: {{ .Name }}
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app.kubernetes.io/name: {{ .Name }}
+      app.kubernetes.io/instance: {{ .Name }}
+  template:
+    metadata:
+      labels:
+        app.kubernetes.io/name: {{ .Name }}
+        app.kubernetes.io/instance: {{ .Name }}
+    spec:
+      containers:
+        - name: nginx
+          image: nginxdemos/hello:latest
+
+          imagePullPolicy: Always
+          ports:
+            - name: http
+              containerPort: 80
+              protocol: TCP`,
+}
+
+func from(raw string, data interface{}) string {
+	tmpl, _ := template.New("tmp").Parse(raw)
+	var w bytes.Buffer
+	tmpl.Execute(&w, data)
+	return string(w.Bytes())
+}
+
+func fromAll(data interface{}) *TemplatesStruct {
+
+	rtn := TemplatesStruct{
+		SslProxyTemplate:   from(RawTemplates.SslProxyTemplate, data),
+		CertTemplate:       from(RawTemplates.CertTemplate, data),
+		ServiceTemplate:    from(RawTemplates.ServiceTemplate, data),
+		DeploymentTemplate: from(RawTemplates.DeploymentTemplate, data),
+	}
+
+	return &rtn
+}
